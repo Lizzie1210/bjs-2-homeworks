77bd00c55a150bc92b7f65fbd33e85bb07813664
Elizaveta M <elizaveta.deb1210@yandex.ru>
2025-08-26 19:33:31 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -19,5 +19,9 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = percent / 100 / 12;
+  let loanBody = amount - contribution;
+  let monthlyPayment = loanBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent)**countMonths) - 1)));
+  let totalAmount = monthlyPayment * countMonths;
+  return Number(totalAmount.toFixed(2));
 }
